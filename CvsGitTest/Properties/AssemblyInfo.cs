6ae1899734a8aad66e3b149da79381e8b9752db2
John Hall <john.hall@camtechconsultants.com>
2013-03-01 16:04:25 +0000

    Initial unit tests for Revision class

--- CvsGitTest/Properties/AssemblyInfo.cs
+++ CvsGitTest/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("CvsGitConverter")]
+[assembly: AssemblyTitle("CvsGitTest")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("CvsGitConverter")]
+[assembly: AssemblyProduct("CvsGitTest")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("ec661d49-de7f-45ce-85a6-e1b2319499f4")]
+[assembly: Guid("6d6e3ee3-2de2-4fea-bcef-d80926eb21a3")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -34,5 +34,3 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-
-[assembly: InternalsVisibleTo("CvsGitTest")]
---
+++ CvsGitTest/RevisionTest.cs
@@ -0,0 +1,53 @@
+﻿/*
+ * John Hall <john.hall@xjtag.com>
+ * Copyright (c) Midas Yellow Ltd. All rights reserved.
+ */
+
+using System;
+using CvsGitConverter;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CvsGitTest
+{
+	[TestClass]
+	public class UnitTest1
+	{
+		[TestMethod]
+		public void IsBranch_NonBranchRevision()
+		{
+			var r = Revision.Create("1.1");
+			Assert.IsFalse(r.IsBranch);
+		}
+
+		[TestMethod]
+		public void IsBranch_BranchRevision()
+		{
+			var r = Revision.Create("1.1.0.2");
+			Assert.IsTrue(r.IsBranch);
+		}
+		
+		[TestMethod]
+		[ExpectedException(typeof(InvalidOperationException))]
+		public void BranchStem_MainRevision()
+		{
+			var r = Revision.Create("1.1");
+			var stem = r.BranchStem;
+		}
+		
+		[TestMethod]
+		public void BranchStem_BranchRevision()
+		{
+			var r = Revision.Create("1.1.2.5");
+			var expected = Revision.Create("1.1.2");
+			Assert.AreEqual(r.BranchStem, expected);
+		}
+		
+		[TestMethod]
+		public void BranchStem_Branchpoint()
+		{
+			var r = Revision.Create("1.1.0.6");
+			var expected = Revision.Create("1.1.6");
+			Assert.AreEqual(r.BranchStem, expected);
+		}
+	}
+}
