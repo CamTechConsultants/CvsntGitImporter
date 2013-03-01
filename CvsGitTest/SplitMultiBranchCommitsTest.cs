e801381fa7951d2b0a5dde228e4626d5d9db949b
John Hall <john.hall@camtechconsultants.com>
2013-03-01 17:54:29 +0000

    Add simple unit test for SplitMultiBranchCommits

---
+++ CvsGitTest/SplitMultiBranchCommitsTest.cs
@@ -0,0 +1,45 @@
+﻿/*
+ * John Hall <john.hall@xjtag.com>
+ * Copyright (c) Midas Yellow Ltd. All rights reserved.
+ */
+
+using System;
+using System.Linq;
+using CvsGitConverter;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CvsGitTest
+{
+	[TestClass]
+	public class SplitMultiBranchCommitsTest
+	{
+		[TestMethod]
+		public void SplitsCommitWithTwoBranches()
+		{
+			var file1 = new FileInfo("file1");
+			var file2 = new FileInfo("file2");
+			file2.AddTag("branch1", Revision.Create("1.1.0.2"));
+
+			var id = "id1";
+			var commit = new Commit(id)
+			{
+				CreateFileRevision(file1, "1.1", id),
+				CreateFileRevision(file2, "1.1.2.1", id),
+			};
+
+			var splitter = new SplitMultiBranchCommits(new[] { commit });
+			var splitCommits = splitter.ToList();
+
+			Assert.AreEqual(splitCommits.Count, 2);
+			Assert.AreEqual(splitCommits.First().Single().File.Name, "file1");
+			Assert.AreEqual(splitCommits.Last().Single().File.Name, "file2");
+		}
+
+
+		private FileRevision CreateFileRevision(FileInfo file, string revision, string commitId)
+		{
+			return new FileRevision(file, Revision.Create(revision), Revision.Empty, DateTime.Now,
+					"fred", commitId);
+		}
+	}
+}
