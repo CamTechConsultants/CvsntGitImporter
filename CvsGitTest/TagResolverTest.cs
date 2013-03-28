cbd251b250cb6f7fc08469f775fd59254e1611e0
John Hall <john.hall@camtechconsultants.com>
2013-03-28 16:43:53 +0000

    Create a test for simple re-ordering case.

--- CvsGitTest/TagResolverTest.cs
+++ CvsGitTest/TagResolverTest.cs
@@ -6,10 +6,8 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-
 using CvsGitConverter;
-using System.Text.RegularExpressions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Rhino.Mocks;
 
 namespace CvsGitTest
@@ -29,7 +27,61 @@ namespace CvsGitTest
 		}
 
 		[TestMethod]
-		public void TagSplitAcrossCommits()
+		public void Resolve_ReorderCommits()
+		{
+			var commits = CreateCommitThatNeedsReordering().ToList();
+			var allFiles = CreateAllFiles(commits);
+
+			var resolver = new TagResolver(m_logger, commits, allFiles, new InclusionMatcher());
+			var result = resolver.Resolve();
+
+			Assert.IsFalse(result, "Failed");
+		}
+
+		[TestMethod]
+		public void ResolveAndFix_ReorderCommits()
+		{
+			var commits = CreateCommitThatNeedsReordering();
+			var orderBefore = commits.ToList();
+			var allFiles = CreateAllFiles(commits);
+
+			var resolver = new TagResolver(m_logger, commits, allFiles, new InclusionMatcher());
+			var result = resolver.ResolveAndFix();
+
+			Assert.IsTrue(result, "Succeeded");
+			Assert.IsTrue(resolver.Commits.SequenceEqual(new[] { orderBefore[0], orderBefore[2], orderBefore[1] }), "Commits reordered");
+		}
+
+
+		private static IEnumerable<Commit> CreateCommitThatNeedsReordering()
+		{
+			var file1 = new FileInfo("file1");
+			file1.AddTag("tag", Revision.Create("1.1"));
+
+			var file2 = new FileInfo("file2");
+			file2.AddTag("tag", Revision.Create("1.2"));
+
+			var id0 = "id0";
+			var commit0 = new Commit(id0)
+			{
+				CreateFileRevision(file1, "1.1", id0),
+				CreateFileRevision(file2, "1.1", id0),
+			};
+			var id1 = "id1";
+			var commit1 = new Commit(id1)
+			{
+				CreateFileRevision(file1, "1.2", id1),
+			};
+			var id2 = "id2";
+			var commit2 = new Commit(id2)
+			{
+				CreateFileRevision(file2, "1.2", id2),
+			};
+
+			return new[] { commit0, commit1, commit2 };
+		}
+
+		private static IEnumerable<Commit> CreateCommitThatNeedsSplitting()
 		{
 			var file1 = new FileInfo("file1");
 			file1.AddTag("tag", Revision.Create("1.2"));
@@ -37,38 +89,38 @@ namespace CvsGitTest
 			var file2 = new FileInfo("file2");
 			file2.AddTag("tag", Revision.Create("1.2"));
 
+			var id0 = "id0";
+			var commit0 = new Commit(id0)
+			{
+				CreateFileRevision(file1, "1.1", id0),
+				CreateFileRevision(file2, "1.1", id0),
+			};
 			var id1 = "id1";
 			var commit1 = new Commit(id1)
 			{
-				CreateFileRevision(file1, "1.1", id1),
-				CreateFileRevision(file2, "1.1", id1),
+				CreateFileRevision(file1, "1.2", id1),
 			};
 			var id2 = "id2";
 			var commit2 = new Commit(id2)
 			{
-				CreateFileRevision(file1, "1.2", id2),
-			};
-			var id3 = "id3";
-			var commit3 = new Commit(id3)
-			{
-				CreateFileRevision(file1, "1.3", id3),
-				CreateFileRevision(file2, "1.2", id3),
+				CreateFileRevision(file1, "1.3", id2),
+				CreateFileRevision(file2, "1.2", id2),
 			};
 
-			var allFiles = new Dictionary<string, FileInfo>()
-			{
-				{ file1.Name, file1 },
-				{ file2.Name, file2 },
-			};
-
-			var resolver = new TagResolver(m_logger, new[] { commit1, commit2, commit3 }, allFiles, new InclusionMatcher());
-			resolver.Resolve();
-
-			Assert.IsTrue(Regex.IsMatch(resolver.Errors.Single(), @"No commit found for tag.*File: file1,r1\.3"));
+			return new[] { commit0, commit1, commit2 };
 		}
 
+		private static Dictionary<string, FileInfo> CreateAllFiles(IEnumerable<Commit> commits)
+		{
+			var allFiles = new Dictionary<string, FileInfo>();
 
-		private FileRevision CreateFileRevision(FileInfo file, string revision, string commitId)
+			foreach (var f in commits.SelectMany(c => c.Select(r => r.File)).Distinct())
+				allFiles.Add(f.Name, f);
+
+			return allFiles;
+		}
+
+		private static FileRevision CreateFileRevision(FileInfo file, string revision, string commitId)
 		{
 			return new FileRevision(file, Revision.Create(revision), Revision.Empty, DateTime.Now,
 					"fred", commitId);
