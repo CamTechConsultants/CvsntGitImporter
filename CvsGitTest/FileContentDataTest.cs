a4c703b8a10ff768b21fa6fe423ba4a675a8ff77
John Hall <john.hall@camtechconsultants.com>
2013-05-10 14:44:37 +0100

    Split FileContentData out into its own file and add unit tests.

---
+++ CvsGitTest/FileContentDataTest.cs
@@ -0,0 +1,64 @@
+﻿/*
+ * John Hall <john.hall@camtechconsultants.com>
+ * Copyright (c) Cambridge Technology Consultants Ltd. All rights reserved.
+ */
+
+using System;
+using CvsGitConverter;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CvsGitTest
+{
+	/// <summary>
+	/// Unit tests for the FileContentData class.
+	/// </summary>
+	[TestClass]
+	public class FileContentDataTest
+	{
+		[TestMethod]
+		public void Equals_SameInstance_ReturnsTrue()
+		{
+			var x = new FileContentData(new byte[] { 1, 2, 3, }, 2);
+
+			Assert.IsTrue(x.Equals(x));
+		}
+
+		[TestMethod]
+		public void Equals_Null_ReturnsFalse()
+		{
+			var x = new FileContentData(new byte[] { 1, 2, 3, }, 2);
+
+			Assert.IsFalse(x.Equals(null));
+		}
+
+		[TestMethod]
+		public void Equals_SameDataAndLength_ReturnsTrue()
+		{
+			var x = new FileContentData(new byte[] { 1, 2, 3, }, 2);
+			var y = new FileContentData(new byte[] { 1, 2, }, 2);
+
+			Assert.IsTrue(x.Equals(y));
+			Assert.IsTrue(y.Equals(x));
+		}
+
+		[TestMethod]
+		public void Equals_SameDataDifferentLength_ReturnsFalse()
+		{
+			var x = new FileContentData(new byte[] { 1, 2, 3, }, 2);
+			var y = new FileContentData(new byte[] { 1, 2, 3, }, 3);
+
+			Assert.IsFalse(x.Equals(y));
+			Assert.IsFalse(y.Equals(x));
+		}
+
+		[TestMethod]
+		public void Equals_DifferentData_ReturnsFalse()
+		{
+			var x = new FileContentData(new byte[] { 1, 2, 3, }, 4);
+			var y = new FileContentData(new byte[] { 2, 3, 4, }, 3);
+
+			Assert.IsFalse(x.Equals(y));
+			Assert.IsFalse(y.Equals(x));
+		}
+	}
+}
---
+++ FileContentData.cs
@@ -0,0 +1,79 @@
+﻿/*
+ * John Hall <john.hall@camtechconsultants.com>
+ * Copyright (c) Cambridge Technology Consultants Ltd. All rights reserved.
+ */
+
+using System;
+using System.Text;
+
+namespace CvsGitConverter
+{
+	/// <summary>
+	/// The raw data for a revision of a file.
+	/// </summary>
+	class FileContentData
+	{
+		/// <summary>
+		/// The data. The buffer may be longer than the file, so the Length field must be used.
+		/// </summary>
+		public readonly byte[] Data;
+
+		/// <summary>
+		/// The length of the file's contents.
+		/// </summary>
+		public readonly long Length;
+
+		public FileContentData(byte[] data, long length)
+		{
+			Data = data;
+			Length = length;
+		}
+
+		public override string ToString()
+		{
+			return Encoding.Default.GetString(Data, 0, (int)Math.Max(Length, 0x100));
+		}
+
+		public override bool Equals(object obj)
+		{
+			return Equals(obj as FileContentData);
+		}
+
+		public bool Equals(FileContentData other)
+		{
+			if (other == null)
+				return false;
+
+			if (this.Length != other.Length)
+				return false;
+
+			if (this.Data == other.Data)
+				return true;
+
+			for (long i = 0; i < Length; i++)
+			{
+				if (this.Data[i] != other.Data[i])
+					return false;
+			}
+
+			return true;
+		}
+
+		public override int GetHashCode()
+		{
+			int byteCount = (int)Math.Min(64, Length);
+			int hashCode = 0;
+			for (int i = 0; i < byteCount; i++)
+			{
+				hashCode ^= Data[i];
+			}
+
+			return hashCode;
+		}
+
+		/// <summary>
+		/// An empty file.
+		/// </summary>
+		public static readonly FileContentData Empty = new FileContentData(new byte[0], 0);
+	}
+}
